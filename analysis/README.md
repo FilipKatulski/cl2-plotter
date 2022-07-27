81b4aa05d62b7f8abad0c8b899e032a76be7445d
Filip Katulski <filip.katulski@cech.ch>
2022-07-27 09:26:03 +0200

    Prepared data analysis module written in Python.

---
+++ analysis/README.md
@@ -0,0 +1,1 @@
+# PyROOT data analysis
---
+++ analysis/plotter.py
@@ -0,0 +1,11 @@
+import os, sys
+
+
+def main():
+    """
+    The main function that parses arguments and runs specified data analysis. 
+    """
+
+
+if __name__ == "__main__":
+    main()
